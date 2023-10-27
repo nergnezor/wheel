8a146a01a4b50c877915afb63373f7e65dca1c25
Erik Rosengren <e@roseng.ren>
2023-10-27 07:43:16 +0200

    Reset

--- player.gd
+++ player.gd
@@ -1,8 +1,8 @@
-extends CharacterBody3D
+extends RigidBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var linear_velocity = Vector3()
+# var linear_velocity = Vector3()
 var start_position = Vector3()
 func _ready():
 	start_position = transform.origin
@@ -12,20 +12,19 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
 	
-	linear_velocity = velocity
-	print(velocity)
 	# Reset to start position if too slow
-	if is_on_floor() and linear_velocity.length() < 10:
-		linear_velocity = Vector3()
-		transform.origin = start_position
-		velocity = Vector3()
-	else:
-		pass
+	# if is_on_floor() and linear_velocity.length() < 10:
+	# 	linear_velocity = Vector3()
+	# 	transform.origin = start_position
+	# 	velocity = Vector3()
+	# else:
+	# 	pass
 
 	# Reset button
-	if Input.is_action_just_pressed("r"):
+	if Input.is_action_just_pressed("ui_focus_next"):
 		print("Reset")
-		linear_velocity = Vector3()
+		# linear_velocity = Vector3()
 		transform.origin = start_position
-		velocity = Vector3()
+		# velocity = Vector3()
+	
 	
