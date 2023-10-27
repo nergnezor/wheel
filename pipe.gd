877d0940d5883dda7ac0bcfd8f6ba3466d69a368
Erik Rosengren <e@roseng.ren>
2023-10-27 20:07:17 +0200

    tarus pipe

--- pipe.gd
+++ pipe.gd
@@ -1,6 +1,6 @@
 extends Node3D
 @onready var pipe = $pipe
-const N_PIPES = 100
+const N_PIPES = 1000
 
 func _ready(): 
 	var last_end_position = pipe.transform.origin + Vector3(0, -10, 0)
@@ -11,9 +11,10 @@ func _ready():
 		
 func duplicate_below(org,i):
 	var another = org.duplicate()
-	another.rotate_x(0.1*i)
-	var y_rotate_offset = 0.2*i
-	var bottom = Vector3(0, -10*i - y_rotate_offset, 0)
+	another.rotate_x(0.01*i)
+	another.transform.origin.z = 0.1*i
+	var y_rotate_offset = 0.01*i
+	var bottom = Vector3(0, -1*i + y_rotate_offset, 0)
 	another.transform.origin = bottom
 	add_child(another)
 
