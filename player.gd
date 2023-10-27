aefae79a54ec1310f0a442eb44a4d984d07552a9
Erik Rosengren <e@roseng.ren>
2023-10-27 10:02:36 +0200

    collision inside pipe

--- player.gd
+++ player.gd
@@ -3,28 +3,19 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 # var linear_velocity = Vector3()
-var start_position = Vector3()
+var start_transform
 func _ready():
-	start_position = transform.origin
-	linear_velocity = Vector3()
+	start_transform = transform
 
 func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
 	
-	# Reset to start position if too slow
-	# if is_on_floor() and linear_velocity.length() < 10:
-	# 	linear_velocity = Vector3()
-	# 	transform.origin = start_position
-	# 	velocity = Vector3()
-	# else:
-	# 	pass
-
 	# Reset button
 	if Input.is_action_just_pressed("ui_focus_next"):
 		print("Reset")
-		# linear_velocity = Vector3()
-		transform.origin = start_position
-		# velocity = Vector3()
+		transform = start_transform
+		var camera = get_node("../camera")
+		camera.transform.origin = start_transform.origin
 	
 	
