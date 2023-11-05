e02cc2ac516f102f7104f05fc842ca710d602a98
Erik Rosengren <e@roseng.ren>
2023-11-05 23:53:09 +0100

    timer

--- player.gd
+++ player.gd
@@ -2,8 +2,10 @@ extends RigidBody3D
 @onready var orb: MeshInstance3D = $orb
 @onready var camera = get_node("../camera")
 @onready var win_label: Label3D = get_node("../camera/win_label")
+@onready var time_label: Label3D = get_node("../camera/time")
 var timer = 0
 const TIMEOUT = 10
+var time_since_start = 0
 
 var paused = false
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -24,13 +26,10 @@ func _physics_process(delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) or Input.is_action_just_pressed("ui_text_clear_carets_and_selection"):
 		reset()
 
-	var rot = get_rotation()
-	# Pause if space is pressed
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) or Input.is_physical_key_pressed(KEY_D):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		apply_torque(Vector3(1*spin_direction,0, 0) * 10)
 		# Set emission color of material
 		material.emission = Color(0.2,0.5,1)
-		
 		was_pressed = true
 	elif was_pressed:
 		material.emission = Color(1,0.8,0)
@@ -39,37 +38,26 @@ func _physics_process(delta):
 		gravity *= -1
 		set_gravity()
 		spin_direction *= -1
-		rotate_progress = 0
-		# Rotate around the y axis
-		# apply_torque(Vector3(0,1,0) * 10)
-		# rotation.y = PI
-	if (rotate_progress < 1):
-#		rotate_y(0.1*PI)
-		# rotate_z(0.1*PI)
-		rotate_progress += 0.1
 	
 	var colliding = get_colliding_bodies()
 	if (colliding.size() > 0):
 		if (colliding[0].get_name() == "goal"):
-			print("Goal")
 			win()
-			# Reset
-#			reset()
-#			return
 		timer = 0
 		return
 	
-	if win_progress < 1:
-		win_progress += 0.1
-		win_label.transform.origin.z += 1
-
-
 	timer += delta
 	if (timer > TIMEOUT):
 		reset()
+	time_since_start += delta
+	var tenths: int = round(time_since_start*10)
+	tenths = tenths % 10
+	var seconds = round(time_since_start)
+	time_label.text = str(seconds)+"."+str(tenths)
 
 func reset():
 	print("Reset")
+	time_since_start = 0
 	spin_direction = 1
 	timer = 0
 	gravity = 9.8
